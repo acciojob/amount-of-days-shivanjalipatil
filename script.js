12a7b14ae50bc2d5e62eabcd05a94891b2e7e163
Shivanjali <119725141+shivanjalipatil@users.noreply.github.com>
2024-06-12 14:12:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function daysOfAYear(y){
+	if(1<y<9999){
+  if(y%400==0 || y%100 !=0 && y%4==0){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+		
+	}
+	
+}
+
+
+
+ let year=prompt("Enter the year");
+let x=daysOfAYear(year);
+console.log(x);
+
